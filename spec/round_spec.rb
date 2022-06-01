1e7fd67e37fab854babce42d81b7c845c8861cf7
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2022-06-01 12:27:18 -0600

    Adds round class and test - incomplete

---
+++ spec/round_spec.rb
@@ -0,0 +1,71 @@
+ require './lib/card'
+ require './lib/turn'
+ require './lib/deck'
+ require './lib/round'
+
+RSpec.describe Round do
+  before(:each) do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck)
+  end
+  it 'exists' do
+    expect(@round).to be_instance_of(Round)
+  end
+
+  it 'it has a deck' do
+    expect(@round.deck).to eq(@deck)
+  end
+
+  it 'has turns' do
+     expect(@round.turns).to eq([])
+  end
+
+  it 'has current card' do
+    expect(@round.current_card).to eq(@card_1)
+  end
+
+  it 'can create a turn' do
+     new_turn = @round.take_turn("Juneau")
+
+     expect(new_turn.class).to eq(Turn)
+     expect(new_turn.correct?).to eq(true)
+     expect(@round.turns).to eq([new_turn])
+  end
+
+  it 'number correct within each round' do
+    require "pry"; binding.pry
+     expect(@round.number_correct).to eq(1)
+  end
+#  round.current_card
+# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
+#
+#  round.take_turn("Venus")
+# #=> #<Turn:0x00007f972a215b38...>
+#
+#  round.turns.count
+# #=> 2
+#
+#  round.turns.last.feedback
+# #=> "Incorrect."
+#
+#  round.number_correct
+# #=> 1
+#
+#  round.number_correct_by_category(:Geography)
+# #=> 1
+#
+#  round.number_correct_by_category(:STEM)
+# #=> 0
+#
+#  round.percent_correct
+# #=> 50.0
+#
+#  round.percent_correct_by_category(:Geography)
+# #=> 100.0
+#
+#  round.current_card
+# #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
+end
